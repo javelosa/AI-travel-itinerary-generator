1d6db2a5260f47fc43e34e8444c7ab7877566ce0
javelosa <49219594+javelosa@users.noreply.github.com>
2025-07-28 16:41:16 +0800

    Added axios and typewriter

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 function displayItinerary(response) {
-  new Typewriter("#travel", {
+  new Typewriter("#itinerary", {
     strings: response.data.answer,
     autoStart: true,
     delay: 10,
@@ -10,16 +10,16 @@ function displayItinerary(response) {
 function generateItinerary(event) {
   event.preventDefault();
 
-  let instructionsInput = document.querySelector("#topic-instructions");
+  let instructionsInput = document.querySelector("#travel-instructions");
   let itineraryElement = document.querySelector("#itinerary");
   let apiKey = "04a6b97ba438oct66060d748685ff445";
   let context =
-    "You are an AI poet and everytime gives you a topic, you tell a four line poem about the topic. Separate each line with a <br />. Make sure to follow the topic instructions.";
-  let prompt = `Topic instructions: Generate a poem about ${instructionsInput.value}.`;
+    "You are an AI travel agent who provides an itinerary for the specific destination and the number of travel days asked. Separate each line with a <br />. Make sure to follow the travel instructions.";
+  let prompt = `Travel instructions: Generate a travel itinerary about ${instructionsInput.value}.`;
   let apiUrl = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`;
 
   itineraryElement.classList.remove("hidden");
-  itineraryElement.innerHTML = `Generating budget travel itinerary about "<strong>${instructionsInput.value}</strong>"<span class="loading-dots"><span>.</span><span>.</span><span>.</span>
+  itineraryElement.innerHTML = `Generating a travel itinerary about "<strong>${instructionsInput.value}</strong>"<span class="loading-dots"><span>.</span><span>.</span><span>.</span>
     </span>`;
 
   //build API URL
@@ -28,5 +28,5 @@ function generateItinerary(event) {
   //Display the generate poem
 }
 
-let travelFormElement = document.querySelector("#travel-creator");
-poemFormElement.addEventListener("submit", generateItinerary);
+let travelFormElement = document.querySelector("#travel-input");
+travelFormElement.addEventListener("submit", generateItinerary);
